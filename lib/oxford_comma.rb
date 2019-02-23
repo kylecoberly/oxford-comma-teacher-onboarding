6fc450acfc5e1b2bb5cf4a56e0f1ca112483a42a
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-23 23:01:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-  array.join(', ').to_s
+  if array.length < 2
+    return array.to_s
+  elsif array.length == 2
+    return array.join(' and ')
+  else
+    last_element = array.pop
+    return "#{array.join(', ')}, and #{last_element}"
+  end
 end
